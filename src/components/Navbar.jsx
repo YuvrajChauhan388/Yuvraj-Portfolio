166a69929bfe8502fac0c4d2e21e1f708dbeffe4
Yuvraj Chauhan <yuvrajchauhan993@gmail.com>
2025-04-14 16:15:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -132,7 +132,7 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-          <img src={logo} alt="Yuvraj's Logo" style={{ height: "40px" }} />
+          <img src={`${process.env.PUBLIC_URL}/Yuvraj_Logo.jpg`} alt="Yuvraj's Logo" style={{ height: "40px" }} />
         </NavLogo>
 
         <MobileIcon onClick={() => setIsOpen(!isOpen)}>
